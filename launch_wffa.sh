346de6c92e75736711b04000e7d72fc8fa4ea47c
Dominik Jain <djain@gmx.net>
2021-02-24 22:22:37 +0100

    Call make_server_cfg using explicit interpreter

--- launch_wffa.sh
+++ launch_wffa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./make_server_cfg.py wffa
+python3.6 make_server_cfg.py wffa
 /home/steam/.steam/steamcmd/steamapps/common/qlds/run_server_x64_minqlx.sh \
 +set fs_homepath /home/steam/.quakelive/wffa \
 +set zmq_stats_password "$1"
