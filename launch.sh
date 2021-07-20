3a79b48c1d5764002379359a030aed8d646f848f
Dominik Jain <djain@gmx.net>
2021-07-20 19:08:41 +0200

    Fixed relative path of zmq_stats_password.txt

--- launch.sh
+++ launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ZMQ_STATS_PW=`cat zmq_stats_password.txt`
-python3 /home/fhammer/quakelive/make_server_cfg.py $1
-python3 /home/fhammer/quakelive/make_mappool_txt.py $1
+ROOT="/home/steam/.quakelive"
+ZMQ_STATS_PW=`cat ${ROOT}/zmq_stats_password.txt`
+python3 ${ROOT}/make_server_cfg.py $1
+python3 ${ROOT}/make_mappool_txt.py $1
 /home/steam/.steam/steamcmd/steamapps/common/qlds/run_server_x64_minqlx.sh \
-+set fs_homepath /home/steam/.quakelive/$1 \
++set fs_homepath ${ROOT}/$1 \
 +set zmq_stats_password "${ZMQ_STATS_PW}"
